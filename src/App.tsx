487f5708f23a97c5dff813d8c8f87f4cfe8b6a69
Haley Boyle <haleyjboyle@gmail.com>
2025-02-17 14:59:21 -0800

    add base path

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
       <Router>
         <AuthProvider>
           <Routes>
+            <Route path="/" element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route element={<PrivateRoute />}>
               <Route path="/dogs" element={<Dogs />} />
