3dbd3a716ca393f7f0148cbb5477a8b30a06de7e
Haley Boyle <haleyjboyle@gmail.com>
2025-02-18 16:21:49 -0800

    last changes

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -5,7 +5,7 @@ import { useAuth } from "./hooks/AuthProvider";
 const PrivateRoute = () => {
   const auth = useAuth();
 
-  if (!auth.isLoggedIn) return <Navigate to="/login" />;
+  if (!auth.isLoggedIn) return <Navigate to="/" />;
   return <Outlet />;
 };
 
